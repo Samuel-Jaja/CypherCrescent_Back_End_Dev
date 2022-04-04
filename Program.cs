42c3a4266dab76438229fe3c2918048a40c6ef4b
Samuel-Jaja <66678860+Samuel-Jaja@users.noreply.github.com>
2022-04-04 03:29:39 +0100

    Update1
    Task on Methods and Recursion

--- Program.cs
+++ Program.cs
@@ -1,335 +1,236 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace CypherCrescentSoftwareAcademy_BackEndCohort
+namespace Methods_Recursion
 {
     internal class Program
     {
+        //Invocation of Methods in main entry
         static void Main(string[] args)
         {
-            //Arrays : Question 1
+            //Method Q1 Call
+            int a, b, c;
+            Console.Write("Enter a : ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter b : ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter c : ");
+            c = Int32.Parse(Console.ReadLine());
 
-            //int[] myarray = new int[20];
+            SolveQuadratic(a, b, c);
 
-            //for (int i = 0; i < myarray.Length; i++)
-            //{
-            //    myarray[i] = i * 5;
-            //    Console.WriteLine(myarray[i]);
-            //}
-
-
-
-
-
-
-
-            //Arrays : Question 2
-           
-            /*
-            bool arraysAreEqual = true;
-
-            Console.Write("Enter length of first array: ");
-            int n = Int32.Parse(Console.ReadLine());
-
-            int[] array1 = new int[n];
-
-            for (int i = 0; i < array1.Length; i++)
-            {
-                Console.Write("Enter element {0}: ", i);
-                array1[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            Console.Write("\nEnter length of second array: ");
-
-            if (n != Int32.Parse(Console.ReadLine()))
-                Console.WriteLine("\nArrays have different lengths.");
-            else
-            {
-                int[] array2 = new int[n];
-
-                for (int i = 0; i < array2.Length; i++)
-                {
-                    Console.Write("Enter element {0}: ", i);
-                    array2[i] = Int32.Parse(Console.ReadLine());
-                }
-
-                for (int i = 0; i < array1.Length; i++)
-                {
-                    if (array1[i] != array2[i])
-                    {
-                        Console.WriteLine("\nArrays are different.");
-                        arraysAreEqual = false;
-                        break;
-                    }
-                }
-
-                if (arraysAreEqual)
-                    Console.WriteLine("\nArrays are the same.");
-            }
-            */
-
-
-
-
-
-
-
-            //Arrays : Question 3
-            /*
+            //Method Q2 Call
+            int num1 = Factorial1(5);
+            Console.WriteLine(num1);
             
-            bool arraysAreEqual = true;
-            char[] array1 = new char[5] { 'a', 'b', 'c', 'd', 'e' };
-            char[] array2 = new char[5] { 'a', 'b', 'c', 'd', 'e' };
+            int num2 = Factorial2(5);
+            Console.WriteLine(num2);
 
-            if (array1.Length > array2.Length) 
-                Console.WriteLine("Second array is lexicographicaly first.");
-            else if (array1.Length < array2.Length) 
-                Console.WriteLine("First array is lexicographicaly first.");
-            else
-            {
-                for (int i = 0; i < array1.Length; i++)
-                {
-                    if (array1[i] < array2[i])
-                    {
-                        Console.WriteLine("First array is lexicographicaly first.");
-                        arraysAreEqual = false;
-                        break;
-                    }
-                    if (array1[i] > array2[i])
-                    {
-                        Console.WriteLine("Second array is lexicographicaly first.");
-                        arraysAreEqual = false;
-                        break;
-                    }
-                }
+            //Method Q3 Call
+            Console.WriteLine(ValuePassed("2"));
 
-                if (arraysAreEqual) 
-                    Console.WriteLine("Arrays are lexicographicaly equal.");
-            }
-            */
-            
+            //Method Q4 Call
+            int data = NumberOfStringChar("Engineering");
+            Console.WriteLine(data);
 
 
 
+            //Recursion Q2 Call
+            string firstname = "CypherCrescent";
+            Console.WriteLine(LengthOfString(firstname));
 
+            //Recursion Q3 Call
+            PrimeNumberGenerator(1, 10);
 
+            //Recursion Q4 Call
 
-
-
-            //Arrays : Question 4
-            
-            int count = 1, tCount = 1, number = 0;
-
-            Console.Write("Enter array length: ");
-            int n = Int32.Parse(Console.ReadLine());
-            int[] arrray = new int[n];
-
-            for (int i = 0; i < arrray.Length; i++)
-            {
-                Console.Write("Enter {0} element: ", i);
-                arrray[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            for (int i = 0; i < arrray.Length - 1; i++)
-            {
-                if (arrray[i] == arrray[i + 1]) tCount++;
-                else tCount = 1;
-
-                if (tCount > count)
-                {
-                    count = tCount;
-                    number = arrray[i];
-                }
-            }
-
-            for (int i = 0; i < count; i++) Console.Write("{0}, ", number);
-
-
-
-
-
-
-
-
-
-            //Arrays : Question 5
-
-
-
-
-
-
-
-
-
-            //Arrays : Question 6
-
-
-
-
-
-            //Arrays : Question 7
-            /*
-            int sum = 0;
-
-            Console.Write("Enter N: ");
-            int n = Int32.Parse(Console.ReadLine());
-            Console.Write("Enter K (K < N): ");
-            int k = Int32.Parse(Console.ReadLine());
-
-            int[] arr = new int[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Enter {0} element: ", i);
-                arr[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            Array.Sort(arr, (a, b) => b.CompareTo(a));
-
-            for (int i = 0; i < k; i++) sum += arr[i];
-
-            Console.WriteLine("\nBiggest sum is {0}", sum);
-            */
-
-
-
-
-
-
-            //Arrays : Question 8
-            /*
-            int a, b, aMin, temporary;
-
-            Console.Write("Enter array length: ");
-            int n = Int32.Parse(Console.ReadLine());
-
-            int[] array1 = new int[n];
-
-            for (a = 0; a < n; a++)
-            {
-                Console.Write("Enter {0} element: ", a);
-                array1[a] = Int32.Parse(Console.ReadLine());
-            }
-
-            for (b = 0; b < n - 1; b++)
-            {
-                aMin = b;
-
-                for (a = b + 1; a < n; a++) 
-                    if (array1[a] < array1[aMin]) 
-                        aMin = a;
-
-                if (aMin != b)
-                {
-                    temporary = array1[b];
-                    array1[b] = array1[aMin];
-                    array1[aMin] = temporary;
-                }
-            }
-
-            for (a = 0; a < n; a++) 
-                Console.Write("{0} ", array1[a]);
-            */
-
-
-
-            //Arrays : Question 9
-
-            /*
-            
-            int sum = 0, tSum;
-
-            Console.Write("Enter array length: ");
-            int n = Int32.Parse(Console.ReadLine());
-
-            int[] array1 = new int[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Enter {0} element: ", i);
-                array1[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            //Traingular Looping and Rectangular Looping 
-
-            for (int i = 0; i < n - 1; i++)
-            {
-                tSum = array1[i];
-
-                for (int j = i + 1; j < n; j++)
-                {
-                    tSum += array1[j];
-                    if (tSum > sum) 
-                        sum = tSum;
-                }
-            }
-
-            Console.WriteLine(" The maximal sum result is {0}. ", sum);
-            */
-
-
-
-
-
-
-            //Arrays : Question 10
-            /*
-            int counter = 0, temporaryCounter = 1, mostFoundNumber = 0;
-
-            Console.Write("Enter array length: ");
-            int n = Int32.Parse(Console.ReadLine());
-
-            int[] array1 = new int[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Enter {0} element: ", i);
-                array1[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            Array.Sort(array1);
-            
-
-            for (int i = 0; i < n - 1; i++)
-            {
-                if (array1[i] == array1[i + 1]) temporaryCounter++;
-                else temporaryCounter = 1;
-                if (temporaryCounter > counter)
-                {
-                    counter = temporaryCounter;
-                    mostFoundNumber = array1[i];
-                }
-            }
-
-            Console.WriteLine("{0} was found {1} times.", mostFoundNumber, counter);
-            */
-
-            //Arrays : Question 11
-
-
-            //Arrays : Question 12
-
-
-
-            //Arrays : Question 13
-
-
-
-            //Arrays : Question 14
-
-
-            //Arrays : Question 15
-            
-            //Arrays : Question 16
-            
-            //Arrays : Question 17
-            
-            //Arrays : Question 18
-            
-            //Arrays : Question 19
-            
-            
 
         }
+
+
+        //Methods Q1
+        public static void SolveQuadratic(double a, double b, double c)
+        {
+            double d = b * b - 4 * a * c;
+            double x, x1, x2, imaginary;
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+               // Console.WriteLine("Two Real Solutions: {0,8:f4} or  {1,8:f4}", x1, x2);
+                Console.WriteLine("Two Real Solutions: {0} or  {1}", x1, x2);
+            }
+            else if (d < 0)
+            {
+                d = -d;
+                x = -b / (2 * a);
+                imaginary = Math.Sqrt(d) / (2 * a);
+                //Console.WriteLine("Two Imaginary Solutions: {0,8:f4} + {1,8:f4} i or {2,8:f4} + {3,8:f4} i", x, img, x, img);
+                Console.WriteLine("Two Imaginary Solutions: {0} + {1} i or {2} + {3} i", x, imaginary, x, imaginary);
+            }
+            else
+            {
+                x = (-b + Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("One Real Solution: {0,8:f4}", x);
+            }
+
+        }
+
+
+        //Methods Q2
+        public static int Factorial1(int number)  //Recursively
+        {
+            if (number <= 1)
+                return 1;
+            return number * Factorial1(number - 1);
+        }
+
+        public static int Factorial2(int number)
+        {
+            if (number <= 1)
+                return 1;
+            int result = 1;
+            for (int i = 2; i <= number; i++) //Iteratively
+            {
+                result = result * i;
+            }
+            return result;
+
+        }
+
+
+        //Methods Q3
+
+        public static string ValuePassed(string Number)
+        {
+            int num = Convert.ToInt32(Number);
+            string name = "";
+            switch (num)
+            {
+                case 1:
+                    name = "One";
+                    break;
+                case 2:
+                    name = "Two";
+                    break;
+                case 3:
+                    name = "Three";
+                    break;
+                case 4:
+                    name = "Four";
+                    break;
+                case 5:
+                    name = "Five";
+                    break;
+                case 6:
+                    name = "Six";
+                    break;
+                case 7:
+                    name = "Seven";
+                    break;
+                case 8:
+                    name = "Eight";
+                    break;
+                case 9:
+                    name = "Nine";
+                    break;
+                case 10:
+                    name = "Ten";
+                    break;
+                case 20:
+                    name = "Twenty";
+                    break;
+                case 30:
+                    name = "Thirty";
+                    break;
+                case 40:
+                    name = "Fourty";
+                    break;
+                case 50:
+                    name = "Fifty";
+                    break;
+                case 60:
+                    name = "Sixty";
+                    break;
+                case 70:
+                    name = "Seventy";
+                    break;
+                case 80:
+                    name = "Eighty";
+                    break;
+                case 90:
+                    name = "Ninety";
+                    break;
+                case 100:
+                    name = "One Hundred";
+                    break;
+            }
+            return name;
+        }
+
+
+        //Methods Q4
+        public static int NumberOfStringChar(string _string)
+        {
+            int length = _string.Length;
+            return length ;
+        }
+
+        //Q5: Remember to read more on Multi Threaded Applications MTA
+        //Ask questions if you dont Understnad
+
+
+        //Recursion Q2
+        static int LengthOfString(string name)
+        {
+            if (name.Equals(""))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1+ LengthOfString(name.Substring(1))  ;
+            }
+        }
+
+        //Recursion Q3
+
+        static void PrimeNumberGenerator(int initial, int final)
+        {
+            if (initial>final || initial == final)
+            {
+                return;
+            }
+            else
+            {
+                List<int> allfactors = new List<int>();
+                for (int i = 1; i <= final; i++)
+                {
+                    if ((initial % i) == 0)
+                    {
+                        allfactors.Add(i);
+                    }
+                }
+                if (allfactors.Count<3)
+                {
+                    for (int i = 0; i < allfactors.Count; i++)
+                    {
+                        Console.WriteLine(allfactors[i]);
+                    }
+                }
+                else
+                {
+                    PrimeNumberGenerator(initial + 1, final);
+                }
+            }
+            
+        }
+
+
+        //Recursion Q4
+
+
+
+
     }
 }
