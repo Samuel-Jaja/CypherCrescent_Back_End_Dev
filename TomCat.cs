9d960a2faea24b01d8aebe8b2fbfb00c48be588c
Samuel-Jaja <66678860+Samuel-Jaja@users.noreply.github.com>
2022-04-10 23:50:54 +0100

    Object Oriented Programming

---
+++ TomCat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter20_Nakov_Question6
+{
+    public class TomCat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("TomCatSound");
+        }
+    }
+}
